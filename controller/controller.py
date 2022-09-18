045cd6f0e6dbe457365230cefa16891a853c19be
William Ledda <villy80@hotmail.it>
2022-09-18 17:36:42 +0200

    Switch between driver and controller

--- controller/controller.py
+++ controller/controller.py
@@ -1,31 +1,22 @@
 import grpc
-from concurrent import futures
+import time
 
 import technics_42099_pb2_grpc as car_grpc
 import technics_42099_pb2 as pb
 
 
-class CarStatusService(car_grpc.CarStatusServicer):
-    def GetBatteryInfo(self, request, context):
-        return pb.BatteryStatus(voltage=7.0, current=0.1)
-
-    def GetSteeringAngle(self, request, context):
-        return pb.SteeringState(angle=0.0)
-
-
-def serve_controller():
-    controller = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    car_grpc.add_CarStatusServicer_to_server(CarStatusService(), controller)
-    controller.add_insecure_port('0.0.0.0:50051')
-
-    try:
-        print("Starting service... ")
-        controller.start()
-        controller.wait_for_termination()
-    except KeyboardInterrupt:
-        print("Stopping service!")
-        controller.stop(grace=True)
+def run():
+    empty = pb.google_dot_protobuf_dot_empty__pb2.Empty()
+    with grpc.insecure_channel('192.168.0.32:50051') as channel:
+        car_service_stub = car_grpc.CarStatusStub(channel)
+        while True:
+            response = car_service_stub.GetBatteryInfo(empty)
+            print(f"Car status: {response.voltage}V {response.current}A")
+            time.sleep(1.0)
 
 
 if __name__ == '__main__':
-    serve_controller()
+    try:
+        run()
+    except KeyboardInterrupt:
+        print("Closing service")
